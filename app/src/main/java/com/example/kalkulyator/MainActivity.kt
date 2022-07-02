bfaee9ba2624b1245b96230f56ccf71dff981d6c
akbaroff077 <shuxratbekovakbarjon3@gmail.com>
2022-07-02 15:48:50 +0500

    The simplest calculator is a four-function calculator that can perform basic arithmetic such as addition, subtraction, multiplication, and division.
    They are sometimes called pocket calculators or handheld electronic calculators because they are small enough to fit in a shirt pocket. They are also the cheapest calculators, costing about $5 or less.
    
    Four-function calculators typically have +, -, x, and / symbols to denote operations and can produce decimal numbers. Some also have a % key, which is used to calculate percentages.
    
    Made by : "Akbaruz".

---
+++ app/src/main/java/com/example/kalkulyator/MainActivity.kt
@@ -0,0 +1,114 @@
+package com.example.kalkulyator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import org.mariuszgromada.math.mxparser.Expression
+import java.text.DecimalFormat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        button_clear.setOnClickListener {
+
+            input.text = ""
+            output.text = ""
+        }
+
+        button_bracket_left.setOnClickListener {
+            input.text = addToInputText("(")
+        }
+        button_bracket_right.setOnClickListener {
+            input.text = addToInputText(")")
+        }
+        button_0.setOnClickListener {
+            input.text = addToInputText("0")
+        }
+        button_1.setOnClickListener {
+            input.text = addToInputText("1")
+        }
+        button_2.setOnClickListener {
+            input.text = addToInputText("2")
+        }
+        button_3.setOnClickListener {
+            input.text = addToInputText("3")
+        }
+        button_4.setOnClickListener {
+            input.text = addToInputText("4")
+        }
+        button_5.setOnClickListener {
+            input.text = addToInputText("5")
+        }
+        button_6.setOnClickListener {
+            input.text = addToInputText("6")
+        }
+        button_7.setOnClickListener {
+            input.text = addToInputText("7")
+        }
+        button_8.setOnClickListener {
+            input.text = addToInputText("8")
+        }
+        button_9.setOnClickListener {
+            input.text = addToInputText("9")
+        }
+        button_dot.setOnClickListener {
+            input.text = addToInputText(".")
+        }
+        button_division.setOnClickListener {
+            input.text = addToInputText("÷")
+        }
+        button_multiply.setOnClickListener {
+            input.text = addToInputText("x")
+        }
+        button_subtraction.setOnClickListener {
+            input.text = addToInputText("-")
+        }
+        button_addition.setOnClickListener {
+            input.text = addToInputText("+")
+        }
+        button_equals.setOnClickListener {
+            showResult()
+        }
+        del.setOnClickListener{
+            input.text = input.text.substring(0, input.text.length -1)
+        }
+    }
+
+    private fun addToInputText(buttonValue: String): String{
+        return "${input.text}$buttonValue"
+    }
+
+    private fun getInputExpression(): String {
+        var expression = input.text.replace(Regex("÷"), "/")
+        expression = expression.replace(Regex("x"), "*")
+        return expression
+    }
+
+    private fun showResult() {
+        try {
+            val expression = getInputExpression()
+            val result = Expression(expression).calculate()
+            if (result.isNaN()) {
+                output.text = "Error"
+                output.setTextColor(ContextCompat.getColor(this, R.color.green))
+                // Show Error Message
+            }else{
+                // Show Result
+                output.text = DecimalFormat("0.######").format(result).toString()
+                output.setTextColor(ContextCompat.getColor(this, R.color.green))
+            }
+
+        } catch (e: Exception) {
+
+            output.text = "Error"
+            output.setTextColor(ContextCompat.getColor(this, R.color.green))
+
+        }
+
+    }
+}
